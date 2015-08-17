28808be0709bb005027f6e42ad73cdac5afbb325
zhangtaichao <taichao1988@foxmail.com>
2015-08-17 21:10:37 +0800

    add qiniu

---
+++ docs/qiuniu/js-sdk-1.0.7-beta/demo/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'ACCESS_KEY': 'AeXxr_RO2G7G273HkoE8hJ4w2cgGfDHGBmqtcRY7',
+    'SECRET_KEY': '5idJ2SRiXonQ2ZUnVG12eKEDn7aeTxg5dWSRpuAG',
+    'Bucket_Name': 'eduonline',
+    'Port': 19110,
+    'Uptoken_Url': '/uptoken',
+    // 'Domain': 'http://qiniu-plupload.qiniudn.com/'
+    'Domain': '7xkw28.com1.z0.glb.clouddn.com'
+};
---
+++ docs/qiuniu/plupload_api.md
@@ -0,0 +1,1 @@
+http://plupload.com/docs/Options
